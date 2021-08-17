b90fe2645a8bb1c71431b80d0cad8797f58ef157
JonelSV <svjonel@gmail.com>
2021-08-17 18:14:42 -0400

    first commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const canvas = document.getElementById("alienCanvas");
+canvas.width = 900;
+canvas.height = 750;
+
+function resize() {
+    const height = window.innerHeight -20;
+    const ratio = canvas.width / canvas.height;
+    const width = height * ratio;
+
+    canvas.style.width = width + "px";
+    canvas.style.height = height + "px";
+}
+
+window.addEventListener("load", resize, false)
+
